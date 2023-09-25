160e077487fcacbf9bdbd09e7108d1fad3aec497
unknown <Academico@D08KETIPC22A303.instructores.senati.local>
2023-09-25 14:52:01 -0500

    Last commit

---
+++ Demo_MVVM/Demo_MVVM/Demo_MVVM.Android/SplashScreen.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demo_MVVM.Droid
+{
+    [Activity(Label = "Escápate - AV", Icon = "@mipmap/ic_plane", Theme = "@style/pantallaCarga",
+    MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize)]
+
+    public class SplashScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+
+            // Create your application here
+        }
+    }
+}
